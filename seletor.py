12edf8de549199e1dbd3ec4171d1dabc7d1f4c70
annanerdola-droid <annanerdola@gmail.com>
2026-05-04 11:29:07 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,8 @@
-#Seletor de Caracteres__
-letra = input("Digite uma letra:").upper()
+print("Seja bem-vindo ao Seletor de caracteres🔠")
+letra = input("Digite uma letra: ").upper()
 if letra == 'A':
-  print("Voce selecionou a opção A")
+  print("Voce selecionou a opção A ✅")
 elif letra == 'B':
-  print("Voce selecionou a opção B")
+  print("Voce selecionou a opção B ✅")
 else:
-  print("Essa opção não esta disponivel")
+  print("Essa opção não esta disponivel ❌")
