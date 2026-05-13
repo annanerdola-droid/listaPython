706ead8dae423c6afd432c2eb0337808ca02a5b5
annanerdola-droid <annanerdola@gmail.com>
2026-05-13 14:32:15 +0000

    v1

--- teste.py
+++ teste.py
@@ -1,1 +1,24 @@
+#crinado um teste com pontuação--
+print("Teste seus Conhecimentos sobre Ciencias🧠")
+pontos = 0
+#--Questão 1--
+print("Qual é a formula da água?💧")
+print("a)H20 \n b)CO2 \n c)AL")
+resposta1 = input("digite a resposta: ").lower()
+if resposta1 == "a":
+    print("correta!!")
+    pontos = pontos + 1
+else:
+    print("você errou!")
+#Questão 2
+print("2-O Sol é☀️: ")
+print("a)Satélite \n b)Estrela \n c)Asteróide")
+resposta2 = input("digite a resposta: ").lower ()
+if resposta2 == "b":
+    print("Você acertou!!")
+    pontos = pontos + 1
+else:
+    print("Você acertou!")
+print("Fim do questionario")
+print(f"Sua pontuação ")
 
